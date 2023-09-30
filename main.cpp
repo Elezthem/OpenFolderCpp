0b2a70b4cb1c0a35b3107275c7ff4be05c9aa264
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-09-30 10:34:43 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	/*ofstream fileOne("index.txt", ios_base::out);
+	if (fileOne.is_open()) {
+		fileOne << "Zaa4eem 4ydick";
+		fileOne.close();
+	}
+	*/
+
+	ifstream fileTwo("index.txt");
+	if (fileTwo.is_open()) {
+		//string temp;
+		//fileTwo >> temp;
+		char temp[100];
+		fileTwo.getline(temp, 100);
+		cout << temp << endl;
+		fileTwo.close();
+	}
+
+	return 0;
+}
